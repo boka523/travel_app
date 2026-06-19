77596c1d327bbb9bae51c53a01482c4bbf38c52d
boka523 <luka.bosnic2@gmail.com>
2026-06-19 12:55:50 +0200

    added app.post("/login") and app.post("/signup")

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,6 @@ app.get("/users", async (req, res) => {
 
 app.post("/trips", async (req, res) => {
   const { user_id,destination, start_date, end_date, transport_type, passengers_num } = req.body;
-
   try {
     const trip = await prisma.trips.create({
       data: {
@@ -48,6 +47,52 @@ app.post("/trips", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Greška kod stvaranja putovanja" });
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  const { name, email, password_hash } = req.body;
+  try{
+    const user = await prisma.users.create({
+      data:{
+        email,
+        name,
+        password_hash
+      }
+    });
+    res.status(201).json(user);
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).json({ error: "Greška kod registracije korisnika" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password_hash } = req.body;
+  try {
+    const user = await prisma.users.findUnique({
+      where: {
+        email: email
+      }
+    });
+    if (!user) {
+      return res.status(404).json({ error: "Korisnik nije pronađen" });
+    }
+    if (user.password_hash !== password_hash) {
+      return res.status(401).json({ error: "Pogrešna lozinka" });
+    }
+    res.status(200).json({
+      message: "Uspješna prijava",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Greška kod prijave korisnika" });
   }
 });
 
