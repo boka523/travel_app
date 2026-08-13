db509508e36270cb46505fe4158647f329a5b0cc
boka523 <luka.bosnic2@gmail.com>
2026-08-13 17:13:23 +0200

    added sort accomodations option

--- frontend/src/components/AddTripForm/miniComponents/AccommodationResults/AccommodationResults.jsx
+++ frontend/src/components/AddTripForm/miniComponents/AccommodationResults/AccommodationResults.jsx
@@ -4,6 +4,7 @@ import dark_left_arrow from "../../../../assets/dark_left_arrow.png";
 import white_left_arrow from "../../../../assets/white_left_arrow.png";
 import dark_right_arrow from "../../../../assets/dark_right_arrow.png";
 import white_right_arrow from "../../../../assets/white_right_arrow.png";
+import Select from "react-select";
 
 const AccommodationResults = ({
   darkMode,
@@ -14,6 +15,121 @@ const AccommodationResults = ({
   setSelectedAccommodation,
   screenWidth,
 }) => {
+  const [sortOption, setSortOption] = useState(null);
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: darkMode ? "white" : "black",
+      border: "none",
+      borderRadius: "30px",
+      height:
+        screenWidth < 800 ? (screenWidth < 400 ? "30px" : "40px") : "40px",
+      width:
+        screenWidth < 1250
+          ? screenWidth < 600
+            ? screenWidth < 400
+              ? "180px"
+              : "210px"
+            : "250px"
+          : "250px",
+      boxShadow: "none",
+      transition: "0.3s",
+      cursor: "pointer",
+    }),
+
+    placeholder: (provided) => ({
+      ...provided,
+      color: darkMode ? "black" : "white",
+      fontSize:
+        screenWidth < 600 ? (screenWidth < 400 ? "16px" : "18px") : "20px",
+      transition: "0.3s",
+      textAlign: "center",
+    }),
+
+    singleValue: (provided) => ({
+      ...provided,
+      color: darkMode ? "black" : "white",
+      fontSize:
+        screenWidth < 600 ? (screenWidth < 400 ? "14px" : "18px") : "20px",
+      transition: "0.3s",
+      textAlign: "center",
+    }),
+
+    menu: (provided) => ({
+      ...provided,
+      marginTop: "10px",
+      borderRadius: "30px",
+      backgroundColor: darkMode ? "white" : "black",
+      transition: "0.3s",
+      overflow: "hidden",
+      width:
+        screenWidth < 1250
+          ? screenWidth < 600
+            ? screenWidth < 400
+              ? "180px"
+              : "210px"
+            : "250px"
+          : "250px",
+    }),
+
+    menuList: (provided) => ({
+      ...provided,
+      padding: 0,
+      transition: "0.3s",
+      backgroundColor: darkMode ? "white" : "black",
+    }),
+
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused
+        ? darkMode
+          ? "#362A19"
+          : "#B6AA99"
+        : darkMode
+          ? "white"
+          : "black",
+      transition: "0.3s",
+      color: state.isFocused
+        ? darkMode
+          ? "white"
+          : "black"
+        : darkMode
+          ? "black"
+          : "white",
+      cursor: "pointer",
+      fontSize:
+        screenWidth < 600 ? (screenWidth < 400 ? "14px" : "16px") : "18px",
+    }),
+
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      transition: "0.3s",
+      color: darkMode ? "white" : "black",
+    }),
+
+    indicatorSeparator: () => ({
+      display: "none",
+      transition: "0.3s",
+    }),
+  };
+
+  const sortOptions = [
+    { value: "priceAsc", label: "Price - ascending" },
+    { value: "priceDesc", label: "Price - descending" },
+  ];
+
+  const sortedAccommodations = [...accommodations].sort((a, b) => {
+    switch (sortOption?.value) {
+      case "priceAsc":
+        return Number(a.totalPrice) - Number(b.totalPrice);
+      case "priceDesc":
+        return Number(b.totalPrice) - Number(a.totalPrice);
+      default:
+        return 0;
+    }
+  });
+
   const [currentAccommodationSlide, setCurrentAccommodationSlide] = useState(0);
   const accommodationsPerSlide =
     screenWidth < 1000
@@ -25,10 +141,10 @@ const AccommodationResults = ({
       : 4;
   const accommodationSlides = Array.from(
     {
-      length: Math.ceil(accommodations.length / accommodationsPerSlide),
+      length: Math.ceil(sortedAccommodations.length / accommodationsPerSlide),
     },
     (_, index) =>
-      accommodations.slice(
+      sortedAccommodations.slice(
         index * accommodationsPerSlide,
         index * accommodationsPerSlide + accommodationsPerSlide,
       ),
@@ -57,6 +173,15 @@ const AccommodationResults = ({
         accommodations.length > 0 && ( //ako se vec loadalo i ako postoji bar jedan smjestaj, prikazi sljedece
           <>
             <h2>Accommodation options:</h2>
+            <Select
+              options={sortOptions}
+              value={sortOption}
+              onChange={setSortOption}
+              className="sort-accommodation"
+              placeholder="Sort by"
+              isSearchable={false}
+              styles={customStyles}
+            />
             <div className="accommodation-slider">
               <button
                 type="button"
